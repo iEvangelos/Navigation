9adce092931d83d6b1a964cdd4d0440ca64acea0
Evan Pipis <ievangelos@icloud.com>
2023-12-23 22:23:39 +0000

    Save NavigationStack paths using Codable • Create @Observable Class • Save/Load the NavigationPath • Bind the path of your NavigationStack to the path property of a PathStore instance.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,37 +7,108 @@
 
 import SwiftUI
 
+// If you're using NavigationPath to store the active path of your NavigationStack, SwiftUI provides two helpers to make saving and loading your paths easier.
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        // Try and load the saved path.
+        if let data = try? Data(contentsOf: savePath) {
+            // We want to decode using a particular type; A Codable Representation of our NavigationPath.
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                // We can use this object to create a NavigationPath.
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+
+        // Still here? Start with an empty NavigationPath.
+        path = NavigationPath()
+    }
+
+    func save() {
+        // NavigationPath can contain non-codable objects. It only needs to comform to Hashable. This check attempts to retrieve the codable NavigationPath out of our data.
+        // This returns nil if at least one object in the path doesn't comform to codable.
+        guard let representation = path.codable else { return }
+
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+// If you're using a homogenous array – e.g. [Int] or [String] – then you don't need those helpers, and you can load or save your data freely.
+@Observable
+class SimplePathStore {
+    var path: [Int] {
+        didSet {
+            save()
+        }
+    }
+    
+    private let simpleSavePath = URL.documentsDirectory.appending(path: "SimpleSavePath")
+    
+    init() {
+        // Try and load the saved path.
+        if let data = try? Data(contentsOf: simpleSavePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        
+        // Still here? Start with an empty array.
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: simpleSavePath)
+        } catch {
+            print("Failed to save Navigation Data")
+        }
+    }
+}
+
+
 // DetailView shows its current number as its title, and has a button that pushes to a new random number whenever it's pressed.
 // Toolbar button to return to the beginning of the Navigation after having gone through multiple views.
 struct DetailView: View {
     var number: Int
-    
-    /* To set the NavigationPath to a new value from the subview, when you don't have access to the original path property you have two options: either store your path in an external class that uses @Observable, or bring in a new property wrapper called @Binding.
-     The @Binding property wrapper lets us pass an @State property into another view and modify it from there – we can share an @State property in several places, and changing it in one place will change it everywhere. */
-    @Binding var path: NavigationPath
-    
+    var simplePathStore: SimplePathStore
+
     var body: some View {
-        NavigationLink("Go to random number", value: Int.random(in: 0...500))
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
             .toolbar {
                 Button("Home") {
-                    path = NavigationPath()
+                    simplePathStore.path = []
                 }
             }
     }
 }
 
+
 struct ContentView: View {
-    // NavigationPath is able to hold a variety of data types in a single path. In practice it works very similarly to an array.
-    // NavigationPath is what we call a type-eraser – it stores any kind of Hashable data without exposing exactly what type of data each item is.
-    @State private var path = NavigationPath()
-    
+    @State private var simplePathStore = SimplePathStore()
+
     var body: some View {
-        NavigationStack(path: $path) {
-            // We can present the DetailView from our ContentView, starting with an initial value of 0 but navigating to a new DetailView every time a new integer is shown.
-            DetailView(number: 0, path: $path)
+        // Bind the path of your NavigationStack to the path property of a PathStore instance.
+        NavigationStack(path: $simplePathStore.path) {
+            DetailView(number: 0, simplePathStore: simplePathStore)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i, simplePathStore: simplePathStore)
                 }
         }
     }
