ca44faf590f7f9094b0d254c3c2d31ddf54965c6
Evan Pipis <ievangelos@icloud.com>
2023-12-23 22:59:15 +0000

    Making the Navigation Title editable • If you're using the .inline title display mode, you can also pass a binding to navigationTitle(). This will then be displayed as usual, but with an important addition: iOS will show a small arrow next to your title, that reveals a "Rename" button to change the title.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,40 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Create @State to hold the value of the NavigationTitle.
+    @State private var title = "SwiftUI"
+
     var body: some View {
         NavigationStack {
-            Text("Hello World")
-                .toolbar {
-                    // If you want multiple buttons using the same placement, you can either repeat ToolbarItem...
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("Leading 1") {
-                            // Button action here.
-                        }
-                    }
-                    
-                    /* Although you can force a button placement, usually it's better to use one of the semantic options – placement that have specific meaning, rather than relying just on their location. These include:
-                     
-                     .confirmationAction, when you want users to agree to something, such as agreeing to terms of service.
-                     .destructiveAction, when the user needs to make a choice to destroy whatever it is they are working with, such as confirming they want to remove some data they created.
-                     .cancellationAction, when the user needs to back out of changes they have made, such as discarding changes they have made.
-                     .navigation, which is used for buttons that make the user move between data, such as going back and forward in a web browser.*/
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("Leading 1") {
-                            // Button action here.
-                        }
-                    }
-                    
-                    //...or you can use ToolbarItemGroup, like this:
-                    ToolbarItemGroup(placement: .topBarTrailing) {
-                        Button("Trailing 1") {
-                            // Button action here.
-                        }
-                        
-                        Button("Trailing 2") {
-                            // Button action here.
-                        }
-                    }
-                }
+            Text("Hello, world!")
+                .navigationTitle($title)
+            // If you're using the .inline title display mode, you can also pass a binding to navigationTitle(). This will then be displayed as usual, but with an important addition: iOS will show a small arrow next to your title, that reveals a "Rename" button to change the title.
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
