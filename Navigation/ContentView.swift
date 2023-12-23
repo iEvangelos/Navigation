102d3aeb776ea0d6d40c1acabbccbdf10a91e2ea
Evan Pipis <ievangelos@icloud.com>
2023-12-23 20:45:20 +0000

    Programmatic navigation with NavigationStack •Created an [Int] to store the paths we'll use for the NavigationStack •Created a NavigationStack and added some buttons that navigation to different views. •Used NavigationDestination to show the new Views.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,28 +7,31 @@
 
 import SwiftUI
 
-//Most of Swift's built-in types already conform to Hashable.
-//If you make a custom struct with properties that all conform to Hashable, you can make the whole struct conform to Hashable with one tiny change.
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
-}
-
 struct ContentView: View {
+    //Create an @State property to store an array of integers that will be used for the path of the NavigationStack
+    @State private var path = [Int]()
+
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                //Now we're using NavigationLink with a NavigationDestination
-                NavigationLink("Select \(i)", value: i)
+        //Bind that property to the path of our NavigationStack, meaning that changing the array will automatically navigate to whatever is in the array, but also changes the array as the user presses Back in the navigation bar.
+        NavigationStack(path: $path) {
+            VStack {
+                //We're setting the whole array so that it just contains the number 32. If anything else happened to be in the array it will be removed, meaning that the NavigationStack will return to its original state before navigating to the number 32.
+                Button("Show 32") {
+                    path = [32]
+                }
+
+                //We're appending 64, meaning that it will be added to whatever we were navigating to. So, if our array already contained 32, we'd now have three views in the stack: the original view (called the "root" view), then something to show the number 32, and finally something to show the number 64.
+                Button("Show 64") {
+                    path.append(64)
+                }
+                
+                //That will present a view for 32 then a view for 64, so the user needs to tap Back twice to get back to the root view.
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                }
             }
-            //The navigationDestination() modifier tell it "when you're asked to navigate to an integer, here's what you should do…"
             .navigationDestination(for: Int.self) { selection in
                 Text("You selected \(selection)")
-            }
-            //This navigationDestination() modifier is similar but for a Student object.
-            .navigationDestination(for: Student.self) { student in
-                Text("You selected \(student.name)")
             }
         }
     }
